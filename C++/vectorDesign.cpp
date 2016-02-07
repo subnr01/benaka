d9e73c6e72146be9408993f8899087de00583c7d
Subbu <subnr01@gmail.com>
2016-02-07 11:24:11 -0500

    Create vectorDesign.cpp

---
+++ C++/vectorDesign.cpp
@@ -0,0 +1,10 @@
+/*
+http://codefreakr.com/how-is-c-stl-implemented-internally/
+http://stackoverflow.com/questions/5159061/implementation-of-vector-in-c
+http://codereview.stackexchange.com/questions/60484/stl-vector-implementation
+https://secweb.cs.odu.edu/~zeil/cs361/web/website/Lectures/vectorImpl/page/vectorImpl.html
+https://www.quora.com/How-is-vector-in-c++-implemented
+https://hadibrais.wordpress.com/2013/11/10/dissecting-the-c-stl-vector-part-1-introduction/
+http://www.softwareandfinance.com/CPP/Vector_Implementation.html
+*/
+
