fbff52fefbd782ca32b56022365b4d2cd7fd432f
Subbu <subnr01@gmail.com>
2016-02-07 11:26:49 -0500

    Update vectorDesign.cpp

--- C++/vectorDesign.cpp
+++ C++/vectorDesign.cpp
@@ -6,5 +6,9 @@ https://secweb.cs.odu.edu/~zeil/cs361/web/website/Lectures/vectorImpl/page/vecto
 https://www.quora.com/How-is-vector-in-c++-implemented
 https://hadibrais.wordpress.com/2013/11/10/dissecting-the-c-stl-vector-part-1-introduction/
 http://www.softwareandfinance.com/CPP/Vector_Implementation.html
+http://upcoder.com/3/roll-your-own-vector
+https://alala666888.wordpress.com/2010/07/17/simple-vector-implementation/
+https://frogatto.com/2009/11/17/how-cs-vector-works-the-gritty-details/
+https://web.stanford.edu/class/archive/cs/cs106b/cs106b.1138/materials/cppdoc/vector-h.html
 */
 
