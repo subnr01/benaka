a7fdbf4e3d94e2e3d7e00ae5564909af484f227d
Subbu <subnr01@gmail.com>
2016-06-12 19:11:45 -0400

    Update auto_ptr.cpp

--- C++/auto_ptr.cpp
+++ C++/auto_ptr.cpp
@@ -1,1 +1,19 @@
 
+/*
+
+Auto_ptr
+https://en.wikipedia.org/wiki/Auto_ptr
+http://www.cplusplus.com/reference/memory/auto_ptr/auto_ptr/
+http://www.gotw.ca/publications/using_auto_ptr_effectively.htm
+http://www.codeproject.com/Articles/23670/auto-ptr-and-its-usage
+http://stackoverflow.com/questions/4554162/is-there-any-reason-to-use-auto-ptr
+http://stackoverflow.com/questions/7867949/sense-of-using-stdauto-ptr
+
+Implementation
+http://stackoverflow.com/questions/4514124/how-could-one-implement-stdauto-ptrs-copy-constructor
+http://www.codeproject.com/Articles/15351/Implementing-a-simple-smart-pointer-in-c
+http://www.aristeia.com/BookErrata/auto_ptr-update.html
+http://venus.cs.qc.edu/~waxman/780/meyersautopointer.pdf
+http://venus.cs.qc.edu/~waxman/780/
+
+*/
