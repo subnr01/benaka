b4b029275b0c2f685d03fd942d40cf2eff4429e7
Subbu <subnr01@gmail.com>
2017-04-25 20:33:40 -0400

    Create concurrent_queue.cpp

---
+++ threads/wisconsin/concurrent_queue.cpp
@@ -0,0 +1,52 @@
+typedef struct __node_t {
+    int                 value;
+    struct __node_t    *next;
+} node_t;
+
+
+typedef struct __queue_t {
+	node_t *head;
+    node_t *tail;	
+    pthread_mutex_t headLock;
+    pthread_mutex_t tailLock;
+} queue_t;
+
+
+
+void Queue_Init(queue_t *q) {
+    node_t *tmp = malloc(sizeof(node_t));
+    tmp->next = NULL;
+    q->head = q->tail = tmp;
+    pthread_mutex_init(&q->headLock, NULL);
+    pthread_mutex_init(&q->tailLock, NULL);
+}
+
+
+
+void Queue_Enqueue(queue_t *q, int value) {
+    node_t *tmp = malloc(sizeof(node_t));
+    assert(tmp != NULL);
+    tmp->value = value;
+    tmp->next  = NULL;
+    pthread_mutex_lock(&q->tailLock);
+    q->tail->next = tmp;
+    q->tail = tmp;
+    pthread_mutex_unlock(&q->tailLock);
+}
+
+
+
+int Queue_Dequeue(queue_t *q, int *value) {
+    pthread_mutex_lock(&q->headLock);
+    node_t *tmp = q->head;
+    node_t *newHead = tmp->next;
+    if (newHead == NULL) {
+        pthread_mutex_unlock(&q->headLock);
+        return -1; // queue was empty
+    }
+    *value = newHead->value;
+    q->head = newHead;
+    pthread_mutex_unlock(&q->headLock);
+    free(tmp);
+    return 0;
+}
