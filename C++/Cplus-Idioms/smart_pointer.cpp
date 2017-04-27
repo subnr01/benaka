e4c261770ae8cc64478830676dff1e535e04ab2c
Subbu <subnr01@gmail.com>
2017-04-27 12:35:15 -0400

    Create smart_pointer.cpp

---
+++ C++/Cplus-Idioms/smart_pointer.cpp
@@ -0,0 +1,7 @@
+/*
+To relieve the burden of duplicating changes to the signature of
+the body class in its handle class when Handle Body idiom or Envelope Letter idiom is in use.
+
+*/
+
+//https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Smart_Pointer
