89de1836fd2e2342dc6c2ff5130743644aa92f43
Subbu <subnr01@gmail.com>
2017-07-27 07:25:11 -0400

    Update elevator.cpp

--- OOAD/elevator.cpp
+++ OOAD/elevator.cpp
@@ -1,3 +1,37 @@
 /*
 Design Elevator using OOAD
 */
+
+
+
+https://stackoverflow.com/questions/493276/modelling-an-elevator-using-object-oriented-analysis-and-design
+
+https://www.quora.com/What-is-the-best-answer-to-Design-an-elevator-system
+
+http://massivetechinterview.blogspot.com/2015/07/thought-works-object-oriented-design.html
+
+http://www.hiringlibrary.com/object-oriented-design-interview-for-parking-lot-or-elevator/ ( good one)
+
+https://softwareengineering.stackexchange.com/questions/336282/design-elevator-system-algorithm-and-classes
+
+https://codereview.stackexchange.com/questions/5723/oop-elevator-design-evaluation
+
+http://thought-works.blogspot.com/2012/11/object-oriented-design-for-elevator-in.html
+
+http://www.web-feats.com/classes/dj/lessons/uml/elevator.htm
+
+https://balaramaraj.wordpress.com/ooad/  (good one)
+
+http://www.sgdevs.com/2013/12/interview-question-design-object.html
+
+
+
+
+https://github.com/joeblau/sample-elevator-control-system
+
+https://github.com/DONGHEHUST/elevatorScheduling/tree/master/src
+
+
+https://github.com/alexakarpov/elevators
+
+https://github.com/kyuseobkim/mesosphere.interview
