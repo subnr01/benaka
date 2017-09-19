8b835e44a9d7227057c6ec5aa5763e16faba600e
Subbu <subnr01@gmail.com>
2017-09-18 20:05:10 -0400

    Create util_endolist.h

---
+++ core/kstl/util_endolist.h
@@ -0,0 +1,25 @@
+
+
+namespace util {
+   struct endolist node;
+   template< class _Tp, endolist_node _Tp::*_nodename> class endolist;
+   template< class _Tp, endolist_node _Tp::*_nodename, class _Ref, class _Ptr> struct _endolist_iterator;
+}
+
+
+struct endolist_node : private Noncopyable
+{
+};
+
+
+struct _endolist_iterator
+{
+
+};
+
+
+class endolist : private Noncopyable
+{
+
+};
+   
