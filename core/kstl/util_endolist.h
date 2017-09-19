f6b4e639feb38849c01d9e815550faff07374aa0
Subbu <subnr01@gmail.com>
2017-09-18 20:24:43 -0400

    Update util_endolist.h

--- core/kstl/util_endolist.h
+++ core/kstl/util_endolist.h
@@ -1,5 +1,9 @@
 
-
+/*
+This class provides a list container for objects that
+contain their own iterator information.
+Semanics similar to the STL list container.
+*/
 namespace util {
    struct endolist node;
    template< class _Tp, endolist_node _Tp::*_nodename> class endolist;
@@ -7,17 +11,42 @@ namespace util {
 }
 
 
+/*
+This must exist as a public data member
+in any class used to comprise a list
+*/
 struct endolist_node : private Noncopyable
 {
+    endolist_node* _M_prev;
+    endolist_node* _M_next;
+    
+    endolist_node() :_M_prev(NULL), _M_next(NULL) {}
+    ~endolist_node() { ASSERT(!is_linked()); }
+    
+    void reset()
+    {
+        _M_prev = _M_next = NULL;
+    }
+    
+    bool is_linked() const 
+    {
+        return _M_next != NULL || _M_prev != NULL;
+    }
+    
 };
 
 
+/*
+   Endolist iterator base class
+*/
 struct _endolist_iterator
 {
 
 };
 
-
+/*
+   Endolist template class
+*/
 class endolist : private Noncopyable
 {
 
