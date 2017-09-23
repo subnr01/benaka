5751b4669470d56ea1fbac8d359dc6d4d71d0b67
Subbu <subnr01@gmail.com>
2017-09-23 10:32:23 -0400

    Update util_autoptr.h

--- core/kstl/smartPointer/util_autoptr.h
+++ core/kstl/smartPointer/util_autoptr.h
@@ -24,3 +24,94 @@ class AutoPtr
 {
   public:
     
+    explicit AutoPtr(T* p = 0) : _ptr(p) {}
+  
+    AutoPtr (AutoPtr &ap) : _ptr(ap.release())  {}
+  
+    template<class D >
+    AutoPtr (AutoPtr<D> &ap) : _ptr(ap.release())  {}
+      
+    AutoPtr (AutoPtrRef<T> ref) : _ptr(ref._ptr)  {}
+    
+    ~AutoPtr() {reset(); }
+  
+    //OPERATORS
+    AutoPtr&  operator=(AutoPtr &rhs)
+    {
+      reset(rhs.release());
+      return *this;
+    }
+  
+    template< class D >
+    AutoPtr&  operator=(AutoPtr<D> &rhs)
+    {
+      reset(rhs.release());
+      return *this;
+    }
+    
+    AutoPtr&  operator=(AutoPtrRef<T> &rhs)
+    {
+      reset(rhs._ptr);
+      return *this;
+    }
+  
+    T* operator->() const
+    {
+      ASSERT(_ptr);
+      return _ptr;
+    }
+  
+    T& operator*() const
+    {
+      ASSERT(_ptr);
+      return *_ptr;
+    }
+  
+    template<class D>
+    operator AutoPtr<D>() { return AutoPtr<D>(release()); }
+  
+    template<class D>
+    operator AutoPtrRef<D>() { return AutoPtrRef<D>(release()); }
+    
+    //OPERATIONS
+    void reset(T *p =0)
+    {
+      if (_ptr != p) {
+        if (_ptr) {
+          delete _ptr;
+        }
+        _ptr = p;
+      }
+    }
+  
+    T* release ()
+    {
+      T *p = _ptr;
+      _ptr = 0;
+      return p;
+    }
+  
+    //ACCESS
+    T *get() const { return _ptr; }
+ 
+  private:
+    //DATA MEMBERS
+    T *ptr;
+  
+};
+
+
+template<class T>
+class AutoPtrRef
+{
+  public:
+    explicit AutoPtrRef(T *p) : _ptr(p) {}
+  
+    T *ptr;
+};
+
+  
+    
+     
+   
+    
