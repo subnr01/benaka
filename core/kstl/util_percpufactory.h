8dbf8054f28405dac934e8e0c435a8656f2712c7
Subbu <subnr01@gmail.com>
2017-09-29 09:54:32 -0400

    Update util_percpufactory.h

--- core/kstl/util_percpufactory.h
+++ core/kstl/util_percpufactory.h
@@ -18,5 +18,37 @@ public:
   PerCpuFactory() {}
   ~PerCpuFactory() { reset(); }
   
+  bool pre_fill(const size_t n = 0) 
+  {
+    int my_proc = sk_my_processor_id();
+    return _arrays[my_proc].populate(n);
+  }
+  
+  T* allocate()
+  {
+    int my_proc = sk_my_processor_id();
+    return _arrays[my_proc].allocate(n);
+  }
+  
+  void recycle(T *t)
+  {
+    int my_proc = sk_my_processor_id();
+    _arrays[my_proc].recycle();
+  }
+  
+  void reset()
+  {
+    for (size_t i = 0; i < PSM_PROCESSORS_MAX; i++)
+    {
+      _arrays[i].reset();
+    }
+  }
+ private:
+  ArrayFactory<T> _arrays[PSM_PROCESSORS_MAX];
+};
+      
+    
+    
+  
   
   
