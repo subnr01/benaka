02657e0b25e36e68f0d3f9ac593ce367757bddd4
Subbu <subnr01@gmail.com>
2017-09-29 12:00:11 -0400

    Update util_arrayfactory.h

--- core/kstl/util_arrayfactory.h
+++ core/kstl/util_arrayfactory.h
@@ -26,3 +26,118 @@ template <class T> class ArrayFactory: private Noncopyable {
 
 
     
+template <class T>
+bool ArrayFactory::resize(const size_t n)
+{
+  if (_capacity >= n) {
+    return true;
+  }
+  
+  T **temp = NULL;
+  
+  const size_t bytes_needed = (n * sizeof(T*));
+  
+  if (IN_WAFL) {
+    DECLARE_CAN_SUSPEND();
+    temp = (T**)wafl_kmem_malloc_common(bytes_needed, NULL, M_UTIL, (uintptr_t)RETURN_ADDRESS);
+  } else {
+    temp = (T**)sk_kmem_malloc(bytes_needed, M_UTIL, SKMA_NOWAIT);
+  }
+  
+  if (temp == NULL) {
+    return false;
+  }
+  
+  if (_pointers != NULL) {
+    for (size_t i = 0; i < count; i++) {
+      temp[i] = _pointers[i];
+      _pointers[i] = NULL;
+    }
+    sk_kmem_free(_pointers);
+  }
+  
+  _pointers = temp;
+  _capacity = n;
+  
+  return true;
+}
+
+template <class T>
+bool ArrayFactory::populate(const size_t n)
+{
+  if (n <= count)
+  {
+    return true;
+  }
+  
+  if (resize(n) == false) {
+    return false;
+  }
+  
+  while(_count < n) {
+    T *temp;
+    temp = new T;
+    if (temp == NULL) {
+      return false;
+    }
+    recycle(temp);
+  }
+  return true;
+}
+
+template<class T>
+void ArrayFactory<T>::recycle(T *element)
+{
+  ASSERT(element != NULL);
+  
+  if(_count >= _capacity) {
+    delete element;
+    return;
+  }
+  
+  _pointers[_count] = element;
+  ++_count;
+}
+
+template<class T>
+T* ArrayFactory<T>::allocate()
+{
+  if(_count == 0) {
+    return NULL;
+  }
+  
+  --_count;
+  
+  T *temp = _pointers[_count];
+  _pointers[_count] = NULL;
+  return temp;
+}
+
+template<class T>
+void ArrayFactory<T>::reset()
+{
+  if (_pointers == NULL)
+  {
+    return;
+  }
+  
+  for(size_t i = 0; i < count; i++)
+  {
+    delete _pointers[i];
+  }
+  
+  _count = 0;
+  _capacity = 0;
+  
+  sk_mem_free(_pointers);
+  _pointers = NULL;
+}
+
+template<class T>
+ArrayFactory<T>::~ArrayFactory()
+{
+  reset();
+}
+
+
+
