ef4414c27ae47dc6cf0f63b9d3501169dbb1cd56
Subbu <subnr01@gmail.com>
2017-09-29 09:57:03 -0400

    Create util_arrayfactory.h

---
+++ core/kstl/util_arrayfactory.h
@@ -0,0 +1,9 @@
+
+/*
+This class exists to provide a fountain of individually usable and 
+freeable objects where a high cycling rate is unlikely. Objects
+allocated through it can be freed through the standard delete
+operator. Objects allocated through the system new operator
+may be freed through it.
+
+*/
