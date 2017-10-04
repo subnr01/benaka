956aba6f91b00374fff44c9465e7fab1b48bf990
Subbu <subnr01@gmail.com>
2017-10-04 15:21:42 -0400

    Create util_bloom.h

---
+++ core/kstl/util_bloom.h
@@ -0,0 +1,10 @@
+/*
+A bloom filter is a fixed-size, lossy set;
+One can insert any number of data values into the filter, and can later
+ask if a particular value is in the set.
+The bloom filter can allow for false positives but
+it will never allow false negatives.
+
+
+
+*/
