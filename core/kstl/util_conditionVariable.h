751e6103daafc976de9dee56909192251b7cdb66
Subbu <subnr01@gmail.com>
2017-10-09 06:24:45 -0400

    Update util_conditionVariable.h

--- core/kstl/util_conditionVariable.h
+++ core/kstl/util_conditionVariable.h
@@ -13,3 +13,42 @@ class ConditionVariable
     {
       pthread_cond_init(&_cv, NULL);
     }
+  
+    ~ConditionVariable()
+    {
+      pthread_cond_destroy(&_cv);
+    }
+  
+    //Atomically unlock the spinlock
+    //and wait till signalled
+    inline void wait(Spinlock *lockp)
+    {
+      lockp->assertLocked();
+      lockp->_isLocked = false;
+      
+      pthread_cond_wait(&_cv, &lockp->_lock);
+      lockp->_isLocked = true;
+    }
+  
+    //wake up one of the waiters
+    //on the condition variable
+    inline void signal()
+    {
+      pthread_cond_signal(&_cv);
+    }
+  
+    //Wake up all the waiters on the
+    //condition variable
+    inline void broadcast()
+    {
+      pthread_cond_broadcast(&_cv);
+    }
+  
+private:
+   #ifdef TEST_SUITE
+    pthread_cond_t _cv;
+  #else
+    sk_Cv cv;
+  #endif
+};  
+    
