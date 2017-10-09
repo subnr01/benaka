82508d1e4316771816018564020b93d0826f7296
Subbu <subnr01@gmail.com>
2017-10-09 06:16:47 -0400

    Create util_conditionVariable.h

---
+++ core/kstl/util_conditionVariable.h
@@ -0,0 +1,9 @@
+/*
+This is a wrapper class around the 
+sk_cv condition variable. Condition variables
+work in conjunction with spinlocks. If threads need
+to suspend in critical regions, then use semaphores instead.
+
+*/
+
+
