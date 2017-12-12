6e3bce65fb3d179d804ee76c9fb6658e588babe0
Subbu <subnr01@gmail.com>
2017-12-12 18:35:48 -0500

    Update make_heap.cpp

--- stl/make_heap.cpp
+++ stl/make_heap.cpp
@@ -4,6 +4,15 @@ make_heap
 A separate heap container class would be redundant since a heap is 
 just a vector in which the values have a certain arrangement. 
 
+A heap is a way to organize the elements of a range that allows for fast 
+retrieval of the element with the highest value at any moment (with pop_heap), 
+even repeatedly, while allowing for fast insertion of new elements (with push_heap)
+
+The element with the highest value is always pointed by first. The order of the other 
+elements depends on the particular implementation, but it is consistent throughout all 
+heap-related functions of this header.
+
+
 make_heap(v.begin(), v.end());
     This algorithm converts the entire vector v into a heap.
     
@@ -74,6 +83,44 @@ initially, v: 3 1 4 1 5 9
 after make_heap, v: 9 5 4 1 1 3 
 largest element: 9
 after removing the largest element, v: 5 3 4 1 1
+*/
+
+// range heap example
+#include <iostream>     // std::cout
+#include <algorithm>    // std::make_heap, std::pop_heap, std::push_heap, std::sort_heap
+#include <vector>       // std::vector
+
+int main () {
+  int myints[] = {10,20,30,5,15};
+  std::vector<int> v(myints,myints+5);
+
+  std::make_heap (v.begin(),v.end());
+  std::cout << "initial max heap   : " << v.front() << '\n';
+
+  std::pop_heap (v.begin(),v.end()); v.pop_back();
+  std::cout << "max heap after pop : " << v.front() << '\n';
+
+  v.push_back(99); std::push_heap (v.begin(),v.end());
+  std::cout << "max heap after push: " << v.front() << '\n';
+
+  std::sort_heap (v.begin(),v.end());
+
+  std::cout << "final sorted range :";
+  for (unsigned i=0; i<v.size(); i++)
+    std::cout << ' ' << v[i];
+
+  std::cout << '\n';
+
+  return 0;
+}
+
+/* 
+output
+initial max heap   : 30
+max heap after pop : 20
+max heap after push: 99
+final sorted range : 5 10 15 20 99
+
 */
 
 
